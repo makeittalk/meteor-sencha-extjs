87d0efdf94b5e1657fd4a6e11412da6965d91d10
jose armando gonzalez lopez <iam@armando.mx>
2013-11-07 00:22:17 -0600

    Created Sencha-Extjs package

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "Simple Sencha ExtJS 4 integration on Meteor"
+});
+
+Package.on_use(function (api, where) {
+  api.use(['templating'], 'client');
+
+  api.add_files('ext-all.js', 'client');
+  api.add_files('sencha.html', 'client');
+});
